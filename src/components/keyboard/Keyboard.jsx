13156f4b27f5b8d9bc01a78a6d73dc7ebf39800c
SamuelHGerber <henrylightfoot@gmail.com>
2021-07-12 07:22:01 -0700

    loops and mute added

--- src/components/keyboard/Keyboard.jsx
+++ src/components/keyboard/Keyboard.jsx
@@ -8,15 +8,29 @@ const fSynth = new Tone.FMSynth().toDestination();
 const nSynth = new Tone.NoiseSynth().toDestination();
 let now = Tone.now();
 
+const loopA = new Tone.Loop((time) => {
+  aSynth.triggerAttackRelease('C2', '8n', time);
+}, '4n').start(0);
+
+const loopB = new Tone.Loop((time) => {
+  fSynth.triggerAttackRelease('C4', '8n', time);
+}, '4n').start(now + 4);
+
 const synthArray = [synth, aSynth, fSynth, nSynth];
 
 const handleToneClick = () => {
-  const currentSynth = synthArray[Math.round(Math.random() * 4)];
-  currentSynth.triggerAttackRelease('C4', now + 1);
-  // currentSynth.triggerRelease(now + 1);
+  // const currentSynth = synthArray[Math.round(Math.random() * 4)];
+  // currentSynth.triggerAttackRelease('C4', now + 1);
+
+  Tone.Transport.start();
+  Tone.Transport.bpm.rampTo(120, 10);
 
   // now = Tone.now();
   // Tone.now gets the current time of AudioContext
+};
+
+const handleMuteClick = () => {
+  Tone.Transport.stop();
 };
 
 export default function Keyboard() {
@@ -25,6 +39,9 @@ export default function Keyboard() {
       <div className={styles.toneTestWrapper}>
         <button className={styles.tone} onClick={handleToneClick}>
           tone
+        </button>
+        <button className={styles.tone} onClick={handleMuteClick}>
+          kill
         </button>
       </div>
       <section className={styles.keyboardWrapper}>
